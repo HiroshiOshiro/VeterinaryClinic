2f229c04beb3f33e31ed9dcaa249a055f7955414
hiroshi oshiro <ooshiro010@gmail.com>
2020-07-05 23:08:18 +0900

    [add][wip]TopView追加

---
+++ VeterinaryClinic/VeterinaryClinic/TopView.swift
@@ -0,0 +1,46 @@
+//
+//  TopView.swift
+//  VeterinaryClinic
+//
+//  Created by hiroshi on 2020/07/05.
+//  Copyright © 2020 hiroshi. All rights reserved.
+//
+
+import SwiftUI
+
+struct TopView: View {
+    var body: some View {
+        VStack(spacing: 10.0) {
+            HStack(spacing: 10.0) {
+                Button(action: {}) {
+                    Text("Chat")
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .frame(height: 60.0)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                Button(action: {}) {
+                    Text("Call")
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .frame(height: 60.0)
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
+            Text("Opening Hours: XXX")
+                .font(.title)
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .padding(.vertical, 20.0)
+                .border(Color.gray, width: 4)
+            
+        }.padding(.horizontal, 10.0)
+    }
+}
+
+struct TopView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopView()
+    }
+}
